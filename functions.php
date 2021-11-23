c502af326e30277dab198103513dff60808b59eb
affian <nnamdiaffia@gmail.com>
2021-11-23 15:34:47 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function renderListing($listing) {
     $propertyType = ucfirst(strtolower($listing->property_type));
     
     echo '<div class="ds-row"><div class="ds-col-6ds-alert ds-success ds-mar-t-1">
-    <p>This is a ' . $propertyType . ' property.<p/>
+    <p>This is a ' . $propertyType . ' property.</p>
     </div></div>';
    
     
