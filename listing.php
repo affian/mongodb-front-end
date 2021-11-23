620caa0296ce9b4317667a2796e6bf32a9ba922f
affian <nnamdiaffia@gmail.com>
2021-11-23 14:25:41 +0000

    Update listing.php

--- listing.php
+++ listing.php
@@ -23,22 +23,7 @@ $rperfresponse = $rperfclient->request('GET', 'findall?id=' . $_GET['id']);
 $content = $rperfresponse->getBody();
 $jsonContent = json_decode($content, false);
 
-echo '<div class="ds-margin-t-b-2">Alternative approach to rendering results on screen
-' . $jsonContent->id . '
-</div>';
-  /*  echo '<div class="ds-row">
-    <div class="ds-col-6 ds-shadow-floating ds-bg-neutral-2">
-    <h3 class="ds-heading-2 ds-margin-t-2">' . $listing->id . ' ' . $listing->name . '</h3>
-    <div class="ds-hr-thick"></div>
-    <h4 class="ds-heading-3 ds-margin-t-b-2">' . $listing->description . '</h4>
-    <p class="ds-margin-b-2">
-    ' . $listing->neighborhood_cleansed . '<br />
-    ' . $listing->accommodates . '<br />
-    ' . $listing->price . '<br />
-    </p>*/
-//print_r($jsonContent);
-//print_r("above is jsonContent below the price & name of this listing.");
-//renderListing($jsonContent);
+echo '<tr>' . $jsonContent->id . '</a></tr>';
 /*
 if ($jsonContent->success == 1) {
     $listing = $jsonContent->data[0];
@@ -54,6 +39,7 @@ if ($jsonContent->success == 1) {
     </div>';
 }
 */ 
+
 // Include a way to return to the previous page
 echo '<br />
 <h4 class="ds-heading-4 ds-margin-t-b-2"><a href="index.php">&lt back to complete listings</a></h4>
