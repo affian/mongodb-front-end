28861a7fd8e09a074881bbdf3ba49e017fc0e679
affian <nnamdiaffia@gmail.com>
2021-11-24 10:58:13 +0000

    Update listing.php

--- listing.php
+++ listing.php
@@ -22,8 +22,9 @@ $rperfclient = new GuzzleHttp\Client([ 'base_uri'=>$apiBaseUri]);
 $rperfresponse = $rperfclient->request('GET', 'findall?id=' . $_GET['id']);
 $content = $rperfresponse->getBody();
 $jsonContent = json_decode($content, true);
-$listing = $jsonContent[0];
-renderListing($listing);
+$listing = $jsonContent[0][];
+print_r($listing);
+//renderListing($listing);
 /*
 if ($jsonContent->success == 1) {
     $listing = $jsonContent->data[0];
