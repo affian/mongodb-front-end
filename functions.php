14a6b2f774b2c5f5b8f99adebc477235d53f749e
affian <nnamdiaffia@gmail.com>
2021-11-24 12:02:35 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function renderListing($listing) {
     <h4 class="ds-heading-3 ds-margin-t-b-2">' . $listing['property_type'] . '</h4>
     <p class="ds-margin-b-2">
     Description: ' . $listing['description'] . '. <br />
-    Neighbourhood Overview: ' . $listing['neighborhood_overview'] . ' <br />
+    Neighbourhood Overview: ' . $listing['neighborhood_overview'] . '. <br />
     Bathrooms: ' . $listing['bathrooms_text'] . ' <br />
     Bedrooms: ' . $listing['bedrooms'] . ' <br />
     Beds: ' . $listing['beds'] . ' <br />
@@ -52,8 +52,8 @@ function renderListing($listing) {
     <div class="ds-table-container">
     <table class="ds-table ds-table-compact">
     <tr><th>Name</th><th>Location</th><th>Response Time</th></tr>
-    <tr><td class="ds-text-align-right">$' . $listing['host_name'] . '</td><td class="ds-text-align-right">$' . $listing['host_location'] . '</td><td class="ds-text-align-right">$' . $listing['host_response_time'] . '</td></tr>
-    <tr><td>&nbsp</td><td class="ds-text-align-right">Acceptance Rate:</td><td class="ds-text-align-right">$' . $listing['host_acceptance_rate'] . '</td></tr>
+    <tr><td class="ds-text-align-right">' . $listing['host_name'] . '</td><td class="ds-text-align-right">' . $listing['host_location'] . '</td><td class="ds-text-align-right">' . $listing['host_response_time'] . '</td></tr>
+    <tr><td>&nbsp</td><td class="ds-text-align-right">Acceptance Rate:</td><td class="ds-text-align-right">' . $listing['host_acceptance_rate'] . '</td></tr>
     </table>
     <p class="ds-margin-b-2">&nbsp</p>
     </div></div></div>
